8bfbc59ebc3930ccf8c745e82215ded66688f6ab
Aditya Thakur <59254164+adityeehaw@users.noreply.github.com>
2022-05-09 22:22:22 +0530

    Delete part2/phonebook directory

--- part2/phonebook/src/components/Persons.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Persons = ({personList}) => {
-    return (
-    personList.map(person => <p key = {person.id}>{person.name} {person.number}</p>)
-    )
-}
-
-export default Persons
